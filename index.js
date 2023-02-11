1cb9c1dbeb8163841de63be622f030d8921d1f72
Sanambir Singh <sanambir123@gmail.com>
2023-02-11 12:19:03 +0530

    set up EJS as view engine

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+// set up express app to use EJS as the template engine
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.send('Hello world!!');
-})
+    res.render('layout', {name: 'world!!'});
+});
 
 app.listen(port, (error) => {
     if (error) {
