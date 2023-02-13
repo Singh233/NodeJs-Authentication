1a5462494e9569214778940a33369ab4631e9cc2
Sanambir Singh <sanambir123@gmail.com>
2023-02-13 12:37:00 +0530

    add google auth strategy

--- routes/users.js
+++ routes/users.js
@@ -19,4 +19,12 @@ router.post('/create-session', passport.authenticate(
 router.get('/sign-out', usersController.destroySession);
 
 
+// passport google oAuth
+router.get('/auth/google', passport.authenticate('google', {scope: ['profile', 'email']}));
+router.get('/auth/google/callback', passport.authenticate('google', {
+    failureRedirect: '/',
+    
+}), usersController.createSession)
+
+
 module.exports = router;
